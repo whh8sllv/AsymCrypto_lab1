1fbefd95f50fcdf5d3732f3cefc2d4096ce68062
Anastasiia Kovalchuk <anastasiia.kovalchuk@factum-ua.com>
2025-10-06 02:52:24 +0300

    + Geffe Generator

--- lab1_main.py
+++ lab1_main.py
@@ -61,6 +61,7 @@ class Generator():
             res.append(x_t)
         return ''.join([str(n) for n in res])
     
+
     '''L89 Generator'''
     def l89_generator(self):
         first_89 = []
@@ -74,12 +75,32 @@ class Generator():
             x_t = res[t-38] ^ res[t-89]
             res.append(x_t)
         return ''.join([str(n) for n in res])
+    
 
+    '''Geffe generator'''
+    def geffe_generator(self):
+        while True:
+            l11 = [random.randint(0, 1) for i in range(11)]
+            l9 = [random.randint(0, 1) for i in range(9)]
+            l10 = [random.randint(0, 1) for i in range(10)]
+            if sum(l11) > 0 and sum(l9) > 0 and sum(l10) > 0:
+                break
+        res_z = []
+        for i in range(self.length):
+            x_i = l11[len(l11)-11] ^ l11[len(l11)-9]
+            y_i = l9[len(l9)-9] ^ l9[len(l9)-8] ^ l9[len(l9)-6] ^ l9[len(l9)-5]
+            s_i = l10[len(l10)-10] ^ l10[len(l10)-7]
 
+            z_i = (s_i & x_i) ^ ((1 ^ s_i) & y_i)
+            res_z.append(z_i)
 
+            l11.append(x_i)
+            l9.append(y_i)
+            l10.append(s_i)
+
+        return ''.join([str(z) for z in res_z])
         
-            
-
+                    
 # python_random_module = Generator(10).python_random_generator()
 # print(python_random_module)
 
@@ -92,6 +113,8 @@ class Generator():
 # generator_l20 = Generator(2300).l20_generator()
 # print(generator_l20)
 
-generator_l89 = Generator(2300).l89_generator()
-print(generator_l89)
+# generator_l89 = Generator(2300).l89_generator()
+# print(generator_l89)
 
+geffe_generator = Generator(1000000).geffe_generator()
+print(geffe_generator)
